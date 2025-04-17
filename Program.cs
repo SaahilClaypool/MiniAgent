50a6734487c6bcc17721cf5e24adeffa2340a7ee
Saahil Claypool <saahil@seekout.com>
2025-04-17 11:31:15 -0700

    Top level edit distance

--- Program.cs
+++ Program.cs
@@ -255,7 +255,11 @@ public class DeveloperPlugin()
             var closestLine = lines
                 .Select(
                     (line, idx) =>
-                        (line, distance: LevenshteinDistance(line.Trim(), firstLine.Trim()), idx)
+                        (
+                            line,
+                            distance: DeveloperPlugin.EditDistance(line.Trim(), firstLine.Trim()),
+                            idx
+                        )
                 )
                 .OrderBy(x => x.distance)
                 .FirstOrDefault();
@@ -285,31 +289,31 @@ public class DeveloperPlugin()
         content = content.Replace(replace, with);
         File.WriteAllText(path, content);
         return $"wrote content to {path}";
+    }
 
-        int LevenshteinDistance(string s, string t)
+    public static int EditDistance(string s, string t)
+    {
+        var d = new int[s.Length + 1, t.Length + 1];
+        for (var i = 0; i <= s.Length; i++)
         {
-            var d = new int[s.Length + 1, t.Length + 1];
-            for (var i = 0; i <= s.Length; i++)
-            {
-                d[i, 0] = i;
-            }
-            for (var j = 0; j <= t.Length; j++)
-            {
-                d[0, j] = j;
-            }
-            for (var i = 1; i <= s.Length; i++)
-            {
-                for (var j = 1; j <= t.Length; j++)
-                {
-                    var cost = s[i - 1] == t[j - 1] ? 0 : 1;
-                    d[i, j] = Math.Min(
-                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                        d[i - 1, j - 1] + cost
-                    );
-                }
-            }
-            return d[s.Length, t.Length];
+            d[i, 0] = i;
         }
+        for (var j = 0; j <= t.Length; j++)
+        {
+            d[0, j] = j;
+        }
+        for (var i = 1; i <= s.Length; i++)
+        {
+            for (var j = 1; j <= t.Length; j++)
+            {
+                var cost = s[i - 1] == t[j - 1] ? 0 : 1;
+                d[i, j] = Math.Min(
+                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                    d[i - 1, j - 1] + cost
+                );
+            }
+        }
+        return d[s.Length, t.Length];
     }
 }
 
