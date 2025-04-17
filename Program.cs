e789be276e605417af01250784850988406d2eca
Saahil Claypool <saahil@seekout.com>
2025-04-17 11:46:09 -0700

    cleanup program

--- Program.cs
+++ Program.cs
@@ -1,19 +1,7 @@
-// See https://aka.ms/new-console-template for more information
-using System.Collections.Concurrent;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-
-Console.WriteLine("Hello, World!");
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(
@@ -21,8 +9,8 @@ var host = Host.CreateDefaultBuilder(args)
         {
             services.AddSingleton<KernelFactory>();
             services.AddScoped<WebPlugin>();
-services.AddScoped<DeveloperPlugin>();
-services.AddScoped<AgentPlugin>();
+            services.AddScoped<DeveloperPlugin>();
+            services.AddScoped<AgentPlugin>();
         }
     )
     .Build();
@@ -30,17 +18,18 @@ var serviceProvider = host.Services;
 var kf = serviceProvider.GetRequiredService<KernelFactory>();
 var kernel = kf.Create(
     LLMModel.Large,
-    typeof(WebPlugin), typeof(DeveloperPlugin), typeof(AgentPlugin)
+    typeof(WebPlugin),
+    typeof(DeveloperPlugin),
+    typeof(AgentPlugin)
 );
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-Console.WriteLine(kernel.Plugins.Skip(1).First().GetType().Name);
 var history = new ChatHistory();
 history.AddSystemMessage(
     """
     You are an agent - please keep going until the user’s query is completely resolved, before ending your turn and yielding back to the user. Only terminate your turn when you are sure that the problem is solved.
     """
 );
-Console.WriteLine("You > ");
+Console.Write("You > ");
 while (Console.ReadLine() is var input && !string.IsNullOrWhiteSpace(input))
 {
     history.AddUserMessage(input);
@@ -55,13 +44,6 @@ while (Console.ReadLine() is var input && !string.IsNullOrWhiteSpace(input))
         Console.Write(token);
     }
     Console.WriteLine();
-    Console.WriteLine($"[{history.Count}] You> ");
+    Console.Write($"[{history.Count}] You> ");
 }
 Console.WriteLine($"Done");
-
-
-
-
-
-
-
