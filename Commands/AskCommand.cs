01e240b95208375834faf87c62c89f66587e0b04
Saahil Claypool <saahil@seekout.com>
2025-05-29 12:31:27 -0700

    cot

--- Commands/AskCommand.cs
+++ Commands/AskCommand.cs
@@ -13,7 +13,16 @@ namespace MyAgent.Commands
     {
         public override async Task<int> ExecuteAsync(CommandContext context, AskSettings settings)
         {
-            var response = await agentPlugin.StartSubtask(settings.Prompt);
+            var prompt = $"""
+            Users's Request: {settings.Prompt}
+
+            If this is a coding question, you should probably follow a pattern like this:
+            - think_tool: <briefly reframe the components to the problem and what you need to look up>
+            - rg_tool: as needed, find the relevant code samples. do NOT just make up code
+            - think_tool: <summarize what code you need to write AT A HIGH LEVEL>
+            - ... do the task
+            """;
+            var response = await agentPlugin.StartSubtask(prompt);
             Console.WriteLine($"Final Response\n\n{response}");
             return 0;
         }
